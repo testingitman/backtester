fb2fd81cd6a955b5ec842b1f07da69975c1b9fee
testingitman <testingitman1@gmail.com>
2025-06-06 22:46:26 +0530

    fix missing DeleteMapping import

--- backend/src/main/java/com/backtester/controller/FeedController.java
+++ backend/src/main/java/com/backtester/controller/FeedController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import redis.clients.jedis.Jedis;
